5e43e5991529e6acc3b8374f979be64f17d74f37
Kartikey Pathak <kartikeypathak08@gmail.com>
2025-01-27 14:41:47 +0530

    Update Mongoose_config.js

--- Mongoose_config.js
+++ Mongoose_config.js
@@ -1,2 +1,4 @@
 const mongoose=require('mongoose');
 mongoose.connect("mongodb+srv://dbkartik:dbkartik7070@created-data.xpiuo.mongodb.net/?retryWrites=true&w=majority&appName=created-data");
+
+//mongodb+srv://dbkartik:<db_password>@created-data.xpiuo.mongodb.net/?retryWrites=true&w=majority&appName=created-data
