7e9cc5058ccd1d0f3954906be3aef0079555979c
Kartikey Pathak <kartikeypathak08@gmail.com>
2025-01-27 14:33:23 +0530

    Update Mongoose_Apis.js

--- Mongoose_Apis.js
+++ Mongoose_Apis.js
@@ -1,5 +1,10 @@
 const express = require("express");
-require('./Mongoose_config');
+// require('./Mongoose_config');
+
+const mongoose=require('mongoose');
+mongoose.connect("mongodb+srv://dbkartik:dbkartik7070@created-data.xpiuo.mongodb.net/?retryWrites=true&w=majority&appName=created-data");
+
+
 const path = require('path');  // Import the 'path' module
 
 const products = require('./Mongoose_collection');
